1f8bdbe1bc58cffc7df58a6b859b00a6d6d7fc9c
Nils Göbel <nils.goebel@student.fhws.de>
2023-10-11 16:51:35 +0200

    refactored image.rs

--- src/image.rs
+++ src/image.rs
@@ -1,13 +1,13 @@
 use std::fs::{read, read_to_string};
 use regex::Regex;
 
-use crate::pixel::Pixel;
-
 #[derive(Debug, PartialEq)]
 pub struct Image {
     height: u16,
     width: u16,
-    data: Vec<Vec<Pixel>>,
+    data1: Vec<Vec<u16>>,
+    data2: Vec<Vec<u16>>,
+    data3: Vec<Vec<u16>>,
 }
 
 impl Image {}
@@ -29,30 +29,48 @@ pub fn read_ppm_from_file(filename: &str) -> Image {
     let height: u16 = dimensions[0].parse().unwrap();
     let width: u16 =  dimensions[1].parse().unwrap();
     let re = Regex::new(r"\s+").unwrap();
-    let mut image_values: Vec<Vec<Pixel>> = vec![];
+
+    let mut image_values1: Vec<Vec<u16>> = vec![];
+    let mut image_values2: Vec<Vec<u16>> = vec![];
+    let mut image_values3: Vec<Vec<u16>> = vec![];
+
     for i in 3..result.len() {
-        let mut row: Vec<Pixel> = vec![];
+        let mut row1: Vec<u16> = vec![];
+        let mut row2: Vec<u16> = vec![];
+        let mut row3: Vec<u16> = vec![];
         let values: Vec<String> = re.split(result[i].as_str()).map(|x| x.to_string()).collect();
         if values.len()/3 != width as usize {
             panic!("Line length to expected width mismatch");
         }
-        // println!("{:?}", values);
+
         for j in (0..values.len()).step_by(3) {
-            let pixel: Pixel = Pixel{one: values[j].parse().unwrap(), two: values[j + 1].parse().unwrap(), three: values[j + 2].parse().unwrap() };
-            row.push(pixel);
+            row1.push(values[j].parse().unwrap());
+            row2.push(values[j+1].parse().unwrap());
+            row3.push(values[j+2].parse().unwrap());
         }
-        image_values.push(row);
+        image_values1.push(row1);
+        image_values2.push(row2);
+        image_values3.push(row3);
+
     }
-    if image_values.len() != height as usize {
-            panic!("row length to expected height mismatch");
+
+    if image_values1.len() != height as usize {
+        panic!("row length to expected height mismatch");
     }
-    Image{height, width, data: image_values}
+    if image_values2.len() != height as usize {
+        panic!("row length to expected height mismatch");
+    }
+    if image_values3.len() != height as usize {
+        panic!("row length to expected height mismatch");
+    }
+
+    Image{height, width, data1: image_values1, data2: image_values2, data3: image_values3}
 }
 
     #[test]
     fn test_ppm_from_file_successful() {
         let read_image = read_ppm_from_file("test/valid_test.ppm");
-        let expected_image = Image { height: 4, width: 4, data: vec![vec![Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 15, two: 0, three: 15 }], vec![Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 15, three: 7 }, Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 0, three: 0 }], vec![Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 15, three: 7 }, Pixel { one: 0, two: 0, three: 0 }], vec![Pixel { one: 15, two: 0, three: 15 }, Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 0, three: 0 }, Pixel { one: 0, two: 0, three: 0 }]]};
+        let expected_image = Image { height: 4, width: 4, data1: vec![vec![0, 0, 0, 15], vec![0, 0, 0, 0], vec![0, 0, 0, 0], vec![15, 0, 0, 0]], data2: vec![vec![0, 0, 0, 0], vec![0, 15, 0, 0], vec![0, 0, 15, 0], vec![0, 0, 0, 0]], data3: vec![vec![0, 0, 0, 15], vec![0, 7, 0, 0], vec![0, 0, 7, 0], vec![15, 0, 0, 0]] };
 
         assert_eq!(expected_image, read_image);
     }
@@ -66,11 +84,11 @@ pub fn read_ppm_from_file(filename: &str) -> Image {
     #[test]
     #[should_panic]
     fn test_ppm_from_file_height_not_as_expected() {
-        let read_image = read_ppm_from_file("test/invalid_test_width_not_equal_to_expected.ppm");
+        let read_image = read_ppm_from_file("test/invalid_test_height_not_equal_to_expected.ppm");
     }
 
     #[test]
     #[should_panic]
     fn test_ppm_from_file_width_not_as_expected() {
-        let read_image = read_ppm_from_file("test/invalid_test_height_not_equal_to_expected.ppm");
+        let read_image = read_ppm_from_file("test/invalid_test_width_not_equal_to_expected.ppm");
     }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use crate::image::read_ppm_from_file;
 
 mod image;
-mod pixel;
 fn main() {
-    let image = read_ppm_from_file("/Users/jogehring/Downloads/boxes_1.ppm");
+    let image = read_ppm_from_file("C:\\Users\\Nils\\IdeaProjects\\jpeg_encoder\\test\\valid_test.ppm");
     println!("{:?}", image);
 }
