fdfa19e88f22d30247692493c66162074f4ba29d
Michael Schmitt <leahcim.s@t-online.de>
2023-10-17 16:51:47 +0200

    cleanup for filewrite test

--- src/bit_stream.rs
+++ src/bit_stream.rs
@@ -118,7 +118,7 @@ mod tests {
     use super::BitStream;
 
     #[test]
-    fn test_flush_to_file() -> std::io::Result<()> {
+    fn test_flush_to_file() {
         let stream = BitStream {
             data: vec![0b10101010, 0b01010101],
             bits_in_last_byte: 0,
@@ -131,8 +131,6 @@ mod tests {
 
         // Clean up the file
         fs::remove_file(filename)?;
-
-        Ok(())
     }
 
     #[test]
